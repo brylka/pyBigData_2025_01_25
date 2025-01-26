f26e25d1397876aee85d6b32329c80d346290411
Bartosz Bryniarski <brylka@jelenia.com>
2025-01-26 13:00:14 +0100

    Niedziela

--- kaf/kafka_producer.py
+++ kaf/kafka_producer.py
@@ -1,5 +1,5 @@
 from confluent_kafka import Producer
-
+from datetime import datetime
 
 class WeatherStationMonitor:
     def __init__(self):
@@ -13,9 +13,20 @@ class WeatherStationMonitor:
         self.monitored_stations.add(station_id)
         print(f"Dodano stację: {station_id}")
 
+    def start_monitoring(self):
+        while True:
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+            for station_id in self.monitored_stations:
+                message = {
+                    'station_id': station_id,
+                    'timestamp': timestamp,
+                    'action': 'fetch_weather'
+                }
+
 
 if __name__ == "__main__":
     monitor = WeatherStationMonitor()
 
     for i in range(1,11):
-        monitor.add_station(f"STACJA{i}")
+        monitor.add_station(f"STACJA{i:03d}")
