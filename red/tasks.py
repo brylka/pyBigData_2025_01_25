22053d8b231768e5bad856f950cc4d20bb405838
Bartosz Bryniarski <brylka@jelenia.com>
2025-01-26 09:55:43 +0100

    Niedziela

--- red/tasks.py
+++ red/tasks.py
@@ -1,10 +1,24 @@
 import requests
 
-station_id = 'STACJA001'
 
-print(f"Pobieranie danych dla stacji {station_id}...")
+def fetch_weather_data(station_id: str):
+    try:
+        print(f"Pobieranie danych dla stacji {station_id}...")
 
-response = requests.get(f"http://127.0.0.1:8000/weather/{station_id}")
+        response = requests.get(f"http://127.0.0.1:8000/weather/{station_id}")
 
-data = response.json()
-print(data)
+        if response.status_code == 200:
+            data = response.json()
+
+            return data
+        else:
+            print(f"Błąd: {response.status_code}")
+            # obsługa błędów
+            return None
+    except Exception as e:
+        print(f"Błąd: {e}")
+        # obsługa błędów
+        return None
+
+
+print(fetch_weather_data("STACJA001"))
